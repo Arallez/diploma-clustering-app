3c40bd77064d76f75a8c426a143352263a35a947
Tigran Minasyan <115475639+Arallez@users.noreply.github.com>
2026-01-14 00:37:48 +0500

    Add URL routes for task list and dynamic challenges

--- apps/simulator/urls.py
+++ apps/simulator/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import simulator_view, challenge_view, RunKMeans, CheckSolution
+from .views import simulator_view, challenge_view, tasks_view, RunKMeans, CheckSolution
 
 urlpatterns = [
     path('', simulator_view, name='simulator_index'),
-    path('challenge/', challenge_view, name='challenge_index'),
+    path('tasks/', tasks_view, name='task_list'),
+    path('challenge/', challenge_view, name='challenge_start'), # По умолчанию первое задание
+    path('challenge/<slug:slug>/', challenge_view, name='challenge_detail'),
+    
     path('api/run-kmeans/', RunKMeans.as_view(), name='run_kmeans'),
     path('api/check-solution/', CheckSolution.as_view(), name='check_solution'),
 ]

354457ce5f45cfefbedd4503ce856e44445d7915
Tigran Minasyan <115475639+Arallez@users.noreply.github.com>
2026-01-14 00:20:13 +0500

    Add URL route for coding challenge page (fix SHA)

--- apps/simulator/urls.py
+++ apps/simulator/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RunKMeans, simulator_view
+from .views import simulator_view, challenge_view, RunKMeans, CheckSolution
 
 urlpatterns = [
     path('', simulator_view, name='simulator_index'),
+    path('challenge/', challenge_view, name='challenge_index'),
     path('api/run-kmeans/', RunKMeans.as_view(), name='run_kmeans'),
+    path('api/check-solution/', CheckSolution.as_view(), name='check_solution'),
 ]

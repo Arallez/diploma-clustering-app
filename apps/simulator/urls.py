8934119b3d126ff7d760b8eac1907fbabec3da56
Tigran Minasyan <115475639+Arallez@users.noreply.github.com>
2026-01-15 01:35:24 +0500

    fix: add namespace='simulator' to urls.py

--- apps/simulator/urls.py
+++ apps/simulator/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import simulator_view, challenge_view, tasks_view, RunKMeans, CheckSolution
 
+app_name = 'simulator'
+
 urlpatterns = [
     path('', simulator_view, name='simulator_index'),
     path('tasks/', tasks_view, name='task_list'),
-    path('challenge/', challenge_view, name='challenge_start'), # По умолчанию первое задание
+    path('challenge/', challenge_view, name='challenge_start'),
     path('challenge/<slug:slug>/', challenge_view, name='challenge_detail'),
     
     path('api/run-kmeans/', RunKMeans.as_view(), name='run_kmeans'),
--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('simulator/', include('apps.simulator.urls')),
+    path('simulator/', include(('apps.simulator.urls', 'simulator'), namespace='simulator')),
     path('', include('apps.core.urls')),
 ]

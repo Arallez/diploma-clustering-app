478d7f1706b89ca189e5c4b6a5e0316cc0261fa0
Tigran Minasyan <115475639+Arallez@users.noreply.github.com>
2026-01-18 23:30:31 +0500

    feat(auth): add secure registration form

---
+++ apps/core/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+        }

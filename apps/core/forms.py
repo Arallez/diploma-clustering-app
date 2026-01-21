9118adf0410eb1c9d8f35cf3fb2b63915f87bf8c
Tigran Minasyan <115475639+Arallez@users.noreply.github.com>
2026-01-21 13:39:53 +0500

    refactor(auth): simplify registration form to username + password only

--- apps/core/forms.py
+++ apps/core/forms.py
@@ -3,11 +3,15 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'password1', 'password2']
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
         }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Remove help text
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None

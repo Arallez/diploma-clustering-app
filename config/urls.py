07ac8f029c448d3185b253947c4345eb33998468
Tigran Minasyan <115475639+Arallez@users.noreply.github.com>
2026-01-28 17:24:16 +0500

    fix(config): register encyclopedia urls in root urlconf

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from apps.core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('simulator/', include(('apps.simulator.urls', 'simulator'), namespace='simulator')),
-    path('', include('apps.core.urls')),
+    path('', core_views.home, name='home'),
+    path('simulator/', include('apps.simulator.urls', namespace='simulator')),
+    path('encyclopedia/', include('apps.encyclopedia.urls', namespace='encyclopedia')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('profile/', core_views.profile, name='profile'),
+    path('register/', core_views.register, name='register'),
 ]

72834cb87f464a88146907516132347652962fcc
Tigran Minasyan <115475639+Arallez@users.noreply.github.com>
2026-01-29 17:34:42 +0500

    feat(api): add runMeanShift wrapper

--- static/js/simulator/api.js
+++ static/js/simulator/api.js
@@ -1,53 +1,110 @@
-export async function runKMeans(points, k) {
-    const response = await fetch('/simulator/api/run-kmeans/', {
+/**
+ * API Wrapper for Clustering Algorithms
+ * Handles all HTTP requests to the Django backend
+ */
+
+const BASE_URL = '/simulator';
+
+// Generic helper for POST requests
+async function postData(endpoint, data) {
+    const response = await fetch(`${BASE_URL}${endpoint}`, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ points, k })
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
     });
     return await response.json();
 }
 
-export async function runDBSCAN(points, eps, minPts) {
-    const response = await fetch('/simulator/api/run-dbscan/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ points, eps, min_pts: minPts })
-    });
+// Generic helper for GET requests
+async function getData(endpoint, params = {}) {
+    const query = new URLSearchParams(params).toString();
+    const response = await fetch(`${BASE_URL}${endpoint}?${query}`);
     return await response.json();
 }
 
-export async function runForel(points, r) {
-    const response = await fetch('/simulator/api/run-forel/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ points, r })
+/**
+ * Run K-Means Algorithm
+ * @param {Array} points - List of {x, y} objects
+ * @param {Number} k - Number of clusters
+ */
+export const runKMeans = async (points, k) => {
+    return await postData('/run/', {
+        algorithm: 'kmeans',
+        points: points,
+        params: { k: k }
     });
-    return await response.json();
-}
+};
 
-export async function runAgglomerative(points, n_clusters) {
-    const response = await fetch('/simulator/api/run-agglomerative/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ points, n_clusters })
+/**
+ * Run DBSCAN Algorithm
+ * @param {Array} points - List of {x, y} objects
+ * @param {Number} eps - Epsilon radius
+ * @param {Number} minPts - Minimum points
+ */
+export const runDBSCAN = async (points, eps, minPts) => {
+    return await postData('/run/', {
+        algorithm: 'dbscan',
+        points: points,
+        params: { eps: eps, minPts: minPts }
     });
-    return await response.json();
-}
+};
 
-export async function getDendrogram(points) {
-    const response = await fetch('/simulator/api/get-dendrogram/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ points })
+/**
+ * Run FOREL Algorithm
+ * @param {Array} points - List of {x, y} objects
+ * @param {Number} radius - Sphere radius (R)
+ */
+export const runForel = async (points, radius) => {
+    return await postData('/run/', {
+        algorithm: 'forel',
+        points: points,
+        params: { radius: radius }
     });
-    return await response.json();
-}
+};
 
-export async function generatePreset(type, nSamples = 100) {
-    const response = await fetch('/simulator/api/generate-preset/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ type, n_samples: nSamples })
+/**
+ * Run Agglomerative (Hierarchical) Algorithm
+ * @param {Array} points - List of {x, y} objects
+ * @param {Number} k - Number of clusters
+ */
+export const runAgglomerative = async (points, k) => {
+    return await postData('/run/', {
+        algorithm: 'agglomerative',
+        points: points,
+        params: { k: k }
     });
-    return await response.json();
-}
+};
+
+/**
+ * Run MeanShift Algorithm
+ * @param {Array} points - List of {x, y} objects
+ * @param {Number} bandwidth - Bandwidth (radius)
+ */
+export const runMeanShift = async (points, bandwidth) => {
+    return await postData('/run/', {
+        algorithm: 'meanshift',
+        points: points,
+        params: { bandwidth: bandwidth }
+    });
+};
+
+/**
+ * Generate Preset Dataset
+ * @param {String} name - Preset name (moons, blobs, circles)
+ * @param {Number} samples - Number of points
+ */
+export const generatePreset = async (name, samples = 100) => {
+    return await getData('/preset/', { name: name, samples: samples });
+};
+
+/**
+ * Get Dendrogram Data
+ * @param {Array} points - List of {x, y} objects
+ */
+export const getDendrogram = async (points) => {
+    return await postData('/dendrogram/', {
+        points: points
+    });
+};

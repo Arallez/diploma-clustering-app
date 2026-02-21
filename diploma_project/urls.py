fd53eaae2d7a3ea92a681a4f4d234b417b6fbbbe
Tigran Minasyan <115475639+Arallez@users.noreply.github.com>
2026-02-22 00:39:21 +0500

    refactor(urls): revert global learning-path as it is now inside core urls

--- diploma_project/urls.py
+++ diploma_project/urls.py
@@ -4,9 +4,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# Import the recommendation view directly
-from encyclopedia.views import get_learning_recommendations
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
@@ -16,9 +13,6 @@ urlpatterns = [
     path('testing/', include('testing.urls')),
     path('tasks/', include('tasks.urls', namespace='tasks')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    
-    # Standalone route for recommendations
-    path('learning-path/', get_learning_recommendations, name='learning_path'),
 ]
 
 if settings.DEBUG:
